ad2c6383c6ffaa1599f2ce09987cb0a2468f50d0
patrick <patrickxandrews@gmail.com>
2021-01-25 14:23:12 -0700

    require token in api calls. todo: refactor token retrieval method

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const stravaApi = require('strava-v3')
 
 const axios = require('axios')
 
-const strava = new stravaApi.client(process.env.STRAVA_ACCESS_TOKEN);
+const strava = new stravaApi.client();
 
 const app = express();
 
@@ -36,28 +36,28 @@ app.listen(PORT, () => {
 
 // get athlete
 app.get('/athlete', (req, res) => {
-    strava.athlete.get({})
+    strava.athlete.get({ access_token: req.query.token })
         .then(val => res.status(200).send(val))
         .catch(err => res.status(err.statusCode).send(err.message))
 });
 
 // get athlete activities
 app.get('/athlete/activities', (req, res) => {
-    strava.athlete.listActivities({ page: 1, perPage: 10 })
+    strava.athlete.listActivities({ page: 1, perPage: 10, access_token: req.query.token })
         .then(val => res.status(200).send(val))
         .catch(err => res.status(err.statusCode).send(err.message))
 });
 
 // get athlete stats by id
 app.get('/athletes/:id/stats', (req, res) => {
-    strava.athletes.stats({ id: req.params.id })
+    strava.athletes.stats({ id: req.params.id, access_token: req.query.token })
         .then(val => res.status(200).send(val))
         .catch(err => res.status(err.statusCode).send(err.message))
 });
 
 // get athlete stats by id
 app.get('/athletes/:id/mile_time_avg', (req, res) => {
-    strava.athletes.stats({ id: req.params.id })
+    strava.athletes.stats({ id: req.params.id, access_token: req.query.token })
         .then(val => {
             const avgTotal = (stats.all_run_totals.moving_time / 60) / (stats.all_run_totals.distance / 1609.344);
             const ytdTotal = (stats.ytd_run_totals.moving_time / 60) / (stats.ytd_run_totals.distance / 1609.344);
@@ -69,6 +69,13 @@ app.get('/athletes/:id/mile_time_avg', (req, res) => {
 
 // request auth token from strava
 app.post('/auth/token', (req, res) => {
+
+    // might be able to refactor to use v3 api
+    // strava.oauth.getRequestAccessURL(args)
+    // strava.oauth.getToken(code,done) (Used to token exchange)
+    // strava.oauth.refreshToken(code) (Callback API not supported)
+    // strava.oauth.deauthorize(args,done)
+
     if (req.body.code)
         axios.post(`https://www.strava.com/oauth/token?client_id=${process.env.STRAVA_CLIENT_ID}&client_secret=${process.env.STRAVA_CLIENT_SECRET}&code=${req.body.code}&grant_type=${req.body.grant_type}`)
             .then(response => {
